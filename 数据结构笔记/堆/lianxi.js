93a4c2f227c1a3b6271f574c068cc3546827cbdd
langrenzhou <3606299751@qq.com>
2022-04-03 14:14:16 +0800

    '20220403堆的修改'

--- 数据结构笔记/堆/lianxi.js
+++ 数据结构笔记/堆/lianxi.js
@@ -11,7 +11,7 @@ class MinHeap{
         if(index == 0) return;
          const i=index;
          const ParentIndex=this.GetParentIndex(i);
-        if(this.heap[ParentIndex] > this.heap[i]){
+        if(this.heap[ParentIndex] < this.heap[i]){
             this.swap(ParentIndex,i);
             this.shiftUp(ParentIndex);//递归的操作，如果上移之后不满足最小堆，那么继续上移
         }
@@ -40,12 +40,12 @@ class MinHeap{
        //下移操作需要获取根节点的左节点和右节点
        const LfetIndex=this.GetLeftIndex(value);
        const RightIndex=this.GetRightIndex(value);
-       if(this.heap[LfetIndex] < this.heap[value]){
+       if(this.heap[LfetIndex] > this.heap[value]){
            //判断左节点是否小于根节点，如果小于根节点则进行换位置
            this.swap(LfetIndex,value);
            this.ShiftDown(LfetIndex);
        }
-       if(this.heap[RightIndex] < this.heap[value]){
+       if(this.heap[RightIndex] > this.heap[value]){
            //判断根节点是否小于右节点如果小于则进行换位置
            this.swap(RightIndex,value);
            this.ShiftDown(RightIndex);
@@ -69,8 +69,8 @@ MinHeaps.insert(5);
 MinHeaps.insert(4);
 MinHeaps.insert(0);
 MinHeaps.pop();
-
-//创建一个最小堆，堆里存储着得分和，名次，判断他们的得分然后排序，堆头就是最后一名，循环只要堆里一直有值就循环下去，三个判断判断是不是三二一，如果那就改写，如果不是就根据坐标，他的值就等于当前的length+1
+MinHeaps.pop();
+console.log(MinHeaps)
 
 
 
